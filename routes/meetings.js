6ba6920c7143be73efac4ac5fbe67ab00df05b68
sjh931125 <sjh931125@naver.com>
2016-12-17 00:01:22 +0900

    모임 수정기능 추가

--- routes/meetings.js
+++ routes/meetings.js
@@ -17,6 +17,12 @@ router.get('/del',function(req,res){
 	});
 });
 
+router.get('/del/:id',function(req,res){
+	Meeting.remove({_id:mongoose.Types.ObjectId(req.params.id)},function(err,output){
+		res.redirect('/');
+	});
+});
+
 
 router.get('/',function(req,res){
 	data={session:null,location:null};
@@ -182,6 +188,111 @@ router.get('/show/:id',function(req,res){
 			data.location=meeting;
 			res.render('meetings/show',data);
 		});
+	}
+});
+
+router.get('/edit/:id',function(req,res){
+	data={session:null,meeting:null};
+	if(!req.session.email) res.redirect('/login');
+	else{
+		data.session={
+			email:req.session.email,
+			name:req.session.name,
+			id:req.session.id,
+			img:req.session.img
+		};
+		Meeting.aggregate([{$lookup:{from:"users",localField:"author",foreignField:"email",as:"user"}},{$match:{_id:mongoose.Types.ObjectId(req.params.id)}}],function(err,meeting){
+			if(meeting){
+				if(req.session.email!=meeting[0].user[0].email){
+					res.redirect('/');
+				}
+				else{
+					data.meeting=meeting;
+					res.render('meetings/edit',data);
+				}
+			}
+			else{
+				res.redirect('/');
+			}
+		});
+	}
+});
+
+router.put('/edit',function(req,res){
+	var form=new multiparty.Form();
+	var _title,_author,_text,_latlng,_address,_date,_time,_img;
+	var now_date=new Date();
+
+	if(!req.session.email) res.redirect('/login');
+	else{
+
+		form.on('field',function(name,value){
+			if(name=="title") _title=value;
+			else if(name=="author") _author=value;
+			else if(name=="text") _text=value;
+			else if(name=="latlng") _latlng=value;
+			else if(name=="address") _address=value;
+			else if(name=="date") _date=value;
+			else if(name=="time") _time=value;
+		});
+
+		form.on('part',function(part){
+			if(!part.filename){
+				part.resume();
+			}
+			else{
+				var ext1=part.filename.split('.');
+				var ext2=ext1[ext1.length-1];
+				var filePath = './public/imgs/user/';
+				var fileName=req.session.email+'.'+ext2;
+				
+				var writeStream = fs.createWriteStream(filePath+fileName);
+				writeStream.filename = fileName;
+				part.pipe(writeStream);
+
+				part.on('end',function(){
+					_img=fileName;
+	                writeStream.end();
+	           });
+			}
+		});
+
+		form.on('close',function(){
+
+			Meeting.findOne({title:_title},function(err,meeting){
+
+					meeting.text=_text;
+					meeting.latlng=_latlng;
+					meeting.address=_address;
+					meeting.date=_date;
+					meeting.time=_time;
+					
+					if(_img){
+						var temp_path='./public/imgs/user/';
+						var temp_ext1=_img.split('.');
+						var temp_ext2=temp_ext1[temp_ext1.length-1];
+						var fileName1=temp_path+_img;
+						var fileName2=temp_path+meeting._id+'.'+temp_ext2;
+						var fileName3=meeting._id+'.'+temp_ext2;
+						fs.rename(fileName1,fileName2);
+						meeting.img=fileName3;
+					}
+					
+
+					meeting.save(function(err){
+						if(err){
+							res.send({"err":1,"msg":err});
+						}
+						else{
+							res.send({"err":0});
+						}
+					})
+
+			});
+
+		});
+
+		form.parse(req);
 	}
 });
 
